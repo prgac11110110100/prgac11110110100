d071702dce6dc2cfb204f4d8ccdc085a79f3a10f
clemente <clemente@god.debian>
2022-01-21 18:24:02 -0300

    create sequence and table

---
+++ create_table.sql
@@ -0,0 +1,14 @@
+-- drop sequence testejava_rastrear;
+CREATE SEQUENCE testejava_rastrear start 2 increment 2;
+
+
+--drop table testejava.rastrear; 
+CREATE TABLE testejava.rastrear (
+  id                SERIAL PRIMARY KEY,
+  id_encomenda      VARCHAR(20),
+  descr_encomenda   VARCHAR(100),
+  nota_fiscal       VARCHAR(15) ,
+  situacao_rastreio varchar(30) , 
+  dt_rastreio       varchar(20) ,
+  usu_rastreio      varchar(15)  
+);      
